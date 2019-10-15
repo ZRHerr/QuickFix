ad2406f951964e3fe42e4c2e885e939f9b465f02
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-10-15 13:25:01 -0400

    CSS/Views
    Added custom CSS and started building some views.

--- PjQuickFix/PjQuickFix/Controllers/UsersController.cs
+++ PjQuickFix/PjQuickFix/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using PjQuickFix.Model;
 
 namespace PjQuickFix.Web
 {
